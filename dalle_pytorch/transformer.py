78b0f763eb29575340db37b05375dde296055955
ethancohen123 <55588944+ethancohen123@users.noreply.github.com>
2021-08-11 10:03:53 +0200

    Update transformer.py

--- dalle_pytorch/transformer.py
+++ dalle_pytorch/transformer.py
@@ -146,16 +146,16 @@ class Transformer(nn.Module):
     
     
     
-    #### For molecules test ##
-    class Mol_Encoder(torch.nn.Module):
-        def __init__(self):
-            super(Mol_Encoder, self).__init__()
-            self.model_mol = RobertaForMaskedLM.from_pretrained("seyonec/PubChem10M_SMILES_BPE_450k")
-            self.model_mol.lm_head=Identity()
+#### For molecules test ##
+class Mol_Encoder(torch.nn.Module):
+    def __init__(self):
+        super(Mol_Encoder, self).__init__()
+        self.model_mol = RobertaForMaskedLM.from_pretrained("seyonec/PubChem10M_SMILES_BPE_450k")
+        self.model_mol.lm_head=Identity()
 
-        def forward(self, input_ids, attention_mask):
-            output_1 = self.model_mol(input_ids=input_ids, attention_mask=attention_mask)
-            hidden_state = output_1[0]
-            pooler = hidden_state[:, 0,:]
+    def forward(self, input_ids, attention_mask):
+        output_1 = self.model_mol(input_ids=input_ids, attention_mask=attention_mask)
+        hidden_state = output_1[0]
+        pooler = hidden_state[:, 0,:]
 
-            return pooler
+        return pooler
